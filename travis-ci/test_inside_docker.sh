dacef9900ef15667e6c70e50605277d7912ae855
Matyas Selmeci <matyas@cs.wisc.edu>
2017-11-28 17:10:41 -0600

    Create a new make_rpms.sh script based on packaging/bamboo-scripts/build-rpms
    Many changes were added to make it run:
    - dropped use of make-packages.pl; instead we get the list of packages
      in dependency order from packaging/etc/packages;
      that file had some errors and a few missing packages so it had to be
      updated
    - CVS-related content was removed
    - Support for old OSs (centos < 6) dropped
    - -C flag added to turn off %check sections in RPM builds to save time
    - 32-bit builds were dropped; may be added back later if I can get them
      to work
    - bamboo-related content was removed
    - package signing was removed (because we don't have the Globus GPG key)
    - SUSE/SLES support was dropped (we don't have SLES container)

--- travis-ci/test_inside_docker.sh
+++ travis-ci/test_inside_docker.sh
@@ -13,7 +13,7 @@ yum clean all
 packages=(gcc gcc-c++ make autoconf automake libtool patch \
           libtool-ltdl-devel openssl openssl-devel git \
           'perl(Test::More)' 'perl(File::Spec)' 'perl(URI)' \
-          file)
+          file sudo)
 
 set +e
 [[ $COMPONENTS == *ssh* ]]     && packages+=(curl)
@@ -22,19 +22,71 @@ set +e
 [[ $COMPONENTS == *gram* ]]    && packages+=('perl(Pod::Html)' bison)
 set -e
 
+if [[ $TASK == rpms ]]; then
+    case $IMAGE in
+        *:centos6)  yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+                    ;;
+        *:centos7)  yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+                    ;;
+    esac
+    packages+=(rpm-build doxygen graphviz 'perl(Pod::Html)')
+    # for globus-gridftp-server:
+    packages+=(fakeroot)
+    # for globus-xio-udt-driver:
+    packages+=(udt udt-devel glib2-devel libnice-devel gettext-devel libffi-devel)
+    # for globus-gram-job-manager:
+    packages+=(libxml2-devel)
+    # for myproxy:
+    packages+=(pam-devel voms-devel cyrus-sasl-devel openldap-devel voms-clients)
+    # for globus-net-manager:
+    packages+=(python-devel)
+    # for globus-gram-audit:
+    packages+=('perl(DBI)')
+    # for globus-scheduler-event-generator:
+    packages+=(redhat-lsb-core)
+fi
+
+
 yum -y -d1 install "${packages[@]}"
 
 getent passwd builduser > /dev/null || useradd builduser
+# builduser will require sudo when building RPMs to do yum installs
+if [[ -d /etc/sudoers.d ]]; then
+    echo "builduser ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/10_builduser
+else
+    echo -e "\nbuilduser ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+fi
 chown -R builduser: /gct
+
+
+print_file_size_table () {
+    (
+        set +x
+        echo '==========================================================================================='
+        echo "                                       $1"
+        echo ' Name                                                                             Size'
+        echo '-------------------------------------------------------------------------------- ----------'
+        find "$2" -type f -printf '%-80f %10s\n' | sort
+    )
+}
+    
 case $TASK in
     (tests)
         su builduser -c "/bin/bash -xe /gct/travis-ci/test_unpriv_inside_docker.sh $IMAGE $COMPONENTS"
-        exit $?
         ;;
     (tarballs)
         cd /gct
         su builduser -c "/bin/bash -xe /gct/travis-ci/make_source_tarballs.sh"
-        exit $?
+        print_file_size_table Tarballs /gct/package-output
+        ;;
+    (rpms)
+        cd /gct
+        su builduser -c "/bin/bash -xe /gct/travis-ci/make_source_tarballs.sh"
+        echo '==========================================================================================='
+        # -C = skip unit tests
+        su builduser -c "/bin/bash -xe /gct/travis-ci/make_rpms.sh -C"
+        print_file_size_table SRPMS /gct/packaging/rpmbuild/SRPMS
+        print_file_size_table RPMS /gct/packaging/rpmbuild/RPMS
         ;;
     (*)
         echo "*** INVALID TASK '$TASK' ***"
