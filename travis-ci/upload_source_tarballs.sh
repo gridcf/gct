b691ac7df88ce8b6ab7ed38c8cc01c22a4867c95
Matyas Selmeci <matyas@cs.wisc.edu>
2018-08-27 16:21:51 -0500

    Elaborate on why hcc-osg-software2.unl.edu

--- travis-ci/upload_source_tarballs.sh
+++ travis-ci/upload_source_tarballs.sh
@@ -9,6 +9,9 @@ repo_owner=$1
 
 keyfile=$(pwd -P)/travis-ci/id_gctuploader
 
+# repo.gridcf.org is an alias for this:
+upload_server=hcc-osg-software2.unl.edu
+
 # obtained by running "ssh-keyscan hcc-osg-software2.unl.edu"
 hostsig="hcc-osg-software2.unl.edu ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2AIWAVx2KY+GhDab9SdxLTvjjzTiNa4pfHe7TvRZ5O+qZNc4c8sBlsG7OZGZvDLMRjGTKFyjJx3jDVUwaf14DwzQi9rgZxEZgBsRFffLATZqz+DyVN1H9uw215pah9Wh6yzaqMn51y6kqg0kk/ip62cYcXFgLKUNkzV0yz5WFugm5ziROZn01v5o74VdCABTAdlZhviUoObCn+bycXoUGGETY5GZ3muAW6y5LydDTD+2S97qJWGdSW7JBIfcmU7n5dl8MrtYKYwGswOgdUDrLtCp6CdZt/Evr+3NyLp35IhLnwxdkBBlKHPY0jXrGHyemsXa0Hq0PG/Ih5d0M8RMp"
 
@@ -34,7 +37,7 @@ sha512sum *.tar.gz > sha512sums
 sftp \
     -o "PubkeyAuthentication=yes" \
     -o "IdentitiesOnly=yes" \
-    -i "$keyfile" -b - gctuploader@hcc-osg-software2.unl.edu <<__END__
+    -i "$keyfile" -b - gctuploader@$upload_server <<__END__
 -mkdir gct6
 cd gct6
 -mkdir sources
